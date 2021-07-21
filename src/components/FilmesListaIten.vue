90eb20109ebc652a50939f6a5a5f8f2eb9774f9f
Francisco-gui <fguilhermedev@gmail.com>
2021-07-21 00:37:15 -0300

    primeiros components

---
+++ src/components/FilmesListaIten.vue
@@ -0,0 +1,26 @@
+<template>
+  <li class="list-group-item">
+    <span>{{ filmeTituloConcatenado }} // {{ ano }}</span>
+    <button class="btn btn-secondary float-right">Selecionar</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    titulo: {
+      type: String,
+      required: true
+    },
+    ano: {
+      type: Number,
+      required:true
+    }
+  },
+  computed: {
+    filmeTituloConcatenado() {
+      return `Título: ${this.titulo}`
+    }
+  },
+}
+</script>
